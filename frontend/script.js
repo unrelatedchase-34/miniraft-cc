74f79c979511a447de703f7126e88a5b411ffc26
unrelatedchase-34 <pes2ug23cs254@pesu.pes.edu>
2026-03-24 12:05:13 +0530

    Frontend: basic drawing working

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,28 @@
- 
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => drawing = true);
+
+canvas.addEventListener("mouseup", () => {
+  drawing = false;
+  ctx.beginPath();
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (!drawing) return;
+
+  const rect = canvas.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  ctx.lineWidth = 2;
+  ctx.lineCap = "round";
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+}); 
