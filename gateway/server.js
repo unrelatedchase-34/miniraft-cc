1d95bb9696afeecd02eb5892d9da3a96ea0592b9
Harini Hegde <pes2ug23cs210@pesu.pes.edu>
2026-03-26 15:08:28 +0530

    feat: gateway WebSocket server with broadcast

--- gateway/server.js
+++ gateway/server.js
@@ -1,1 +1,29 @@
- 
+const WebSocket = require('ws');
+
+const PORT = 3000;
+const wss = new WebSocket.Server({ port: PORT });
+
+let clients = new Set();
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  clients.add(ws);
+
+  ws.on('message', (data) => {
+    console.log('Received:', data.toString());
+
+    // Broadcast to all connected clients
+    for (let client of clients) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data.toString());
+      }
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    clients.delete(ws);
+  });
+});
+
+console.log(`Gateway WebSocket server running on ws://localhost:${PORT}`);
